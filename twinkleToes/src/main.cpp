aeec3bcda41f9f6f9e325f32b1aed0a5f94aa365
pl1824 <patricktonylee@gmail.com>
2020-06-20 13:31:26 -0400

    .

--- twinkleToes/src/main.cpp
+++ twinkleToes/src/main.cpp
@@ -1,58 +1,38 @@
 #include <Arduino.h>
 #include <FastLED.h>
-#include"ledFunctions.h"
+#include<Thread.h>
+#include<ThreadController.h>
 
-FASTLED_USING_NAMESPACE
-
-//abandoned 
-
-//rewrite with define macro if u need space, pin 3 is reserved 
-uint8_t sensorPins[] = {2}; 
+#include"lightThreads.h"
 
 CRGB leds[NUM_LEDS];
-CRGB ledCopy[NUM_LEDS];
 
-//same indexes for the two lists
-uint8_t operations[4][OPERATIONS_LEN];
-unsigned long debounceTimers[OPERATIONS_LEN]; 
+ThreadController threadcontroller(100);
 
-uint8_t gHue = 0;
+DigitalSensor sensor1(2,&threadcontroller);
+
+AnalogSensor sensor2(A2,&threadcontroller);
+
+void darken(){
+  fadeToBlackBy(leds, NUM_LEDS, 10);
+}
 
 void setup() {
+
   delay(2000); //sanity check, youve got 2 seconds to get ur shit together
 
   FastLED.addLeds<WS2811, DATA_PIN, RGB>(leds, NUM_LEDS);
   FastLED.clear();
   FastLED.setBrightness(BRIGHTNESS);
 
-  //setup inputs
-  for(uint8_t i = 0; i < sizeof(sensorPins); i++){
-    pinMode(sensorPins[i], INPUT);
-    };
-
-  //set debounceTimers to ;
-  for(uint8_t i = 0; i < OPERATIONS_LEN; i++){
-    debounceTimers[i] = 0;
-    };
-  
-  //set row[0] to leds length
-  for(uint8_t i = 1; i < OPERATIONS_LEN; i++){
-    operations[0][i] = NUM_LEDS;
-    };
-  
-  //set operations' index 0 to 1
-  for(uint8_t i = 0; i < 3; i++){
-    operations[i][0] = 1;
-    };
+ 
 }
 
 void loop(){
+  sensor1.run();
+  sensor2.run();
+  
+  threadcontroller.run();
 
-  if(debounceInput(2,COLOR_RUN_INDEX)){
-    colorRun();
-  }
-
-  EVERY_N_MILLISECONDS(100){
-    runOperations();
-  }
+  FastLED.show();
 }
